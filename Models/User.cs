47a0043b1f0106bc7c25ecdb9f7b241065203b6f
claudiacazacu <claudia-mihaela.cazacu@s.unibuc.ro>
2026-05-23 23:52:19 +0300

    fixed User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -1,4 +1,4 @@
-using System;
+using System.ComponentModel.DataAnnotations;
 namespace instagram.Models
 {
     public class User
@@ -6,9 +6,14 @@ namespace instagram.Models
         public int Id { get; set;} //PK-ul
         public string Nume {get; set;} = null!;
         public string Prenume {get; set;} = null!;
+        [Required(ErrorMessage = "Email obligatoriu")]
         public string Email {get; set;} = null!;
+        [Required(ErrorMessage = "Username obligatoriu")]
         public string Username{get; set;} = null!;
+        [Required(ErrorMessage = "Parola obligatorie")]
         public string Parola {get; set;} = null!;
         public string DataNasterii {get; set;} = null!;
+        public ICollection<Post> Posts { get; set; } = new List<Post>();
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
     }
 }

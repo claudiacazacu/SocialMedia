3fdb7961d424398c72fb78e2481ee91db906bd28
claudiacazacu <claudia-mihaela.cazacu@s.unibuc.ro>
2026-05-23 23:48:03 +0300

    add Comment.cs

---
+++ Models/Comment.cs
@@ -0,0 +1,19 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+namespace instagram.Models
+{
+    public class Comment
+    {
+        public int Id {get; set;}
+        [Required(ErrorMessage = "Comentariul nu poate sa fie gol")]
+        public string Content {get; set;}
+        public DateTime Date {get; set;}
+        [Required(ErrorMessage = "Utilizatorul nu poate sa fie gol")]
+        public int UserId {get; set;} //FK-ul
+        public int PostId {get; set;} //FK-ul
+        public Post Post { get; set; } = null!;
+        
+        // cand adaug clasa User, decomentez aici asta:
+        // public User User { get; set; } = null!;
+    }
+}

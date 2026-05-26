9a12062b47396415274bdf03a01c21687a9717dd
claudiacazacu <claudia-mihaela.cazacu@s.unibuc.ro>
2026-05-26 11:45:57 +0300

    added admininfo 4 login

---
+++ Migrations/20260526083400_AddIdentityJWT.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace instagram.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIdentityJWT : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
